e9fc4ee39cfdc0ce7206cd3a4952d37df9ab5756
jonkrause <jonathonkrause@gmail.com>
2017-07-06 16:49:52 -0400

    add item with prompt

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ var cheese = {
 }
 
 var array01 = [];
-
 array01.splice(0, 0, eggs, milk, cheese);
 console.log(array01);
 
 
+
+// var arrayPrice = [eggs.price, milk.price, cheese.price];
+// console.log(arrayPrice.);
+
+// console.log(array01[1].price);
 
 var subTotal = (eggs.price + milk.price + cheese.price);
 var salesTax = .06;
@@ -26,3 +30,15 @@ var total = ((subTotal * salesTax) + subTotal);
 console.log("Sub-Total: " + subTotal);
 console.log("Sales Tax: " + (subTotal * salesTax).toFixed(2));
 console.log("Total: " + total.toFixed(2));
+
+
+
+// add to list with prompt
+
+var newItem = prompt("new item");
+var newPrice = prompt("new price");
+array01.push({name: newItem, price: newPrice});
+console.log(array01);
+
+// update total
+console.log("Updated Total: " + total.toFixed(2));
