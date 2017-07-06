d854240987373da99da6a8b8cf1a77b6817c6208
jonkrause <jonathonkrause@gmail.com>
2017-07-06 17:54:10 -0400

    worked on total price updates

--- script.js
+++ script.js
@@ -15,16 +15,24 @@ var cheese = {
 
 var array01 = [];
 array01.splice(0, 0, eggs, milk, cheese);
-console.log(array01);
+// console.log(array01);
 
 
 
-// var arrayPrice = [eggs.price, milk.price, cheese.price];
-// console.log(arrayPrice.);
+var subTotal;
 
-// console.log(array01[1].price);
+function runTotal() {
+    subTotal = 0;
+    array01.forEach(function (food) {
+        subTotal += food.price;
+        // console.log(food.price);
 
-var subTotal = (eggs.price + milk.price + cheese.price);
+    })
+}
+
+runTotal();
+
+
 var salesTax = .06;
 var total = ((subTotal * salesTax) + subTotal);
 console.log("Sub-Total: " + subTotal);
@@ -33,12 +41,26 @@ console.log("Total: " + total.toFixed(2));
 
 
 
-// add to list with prompt
 
+// add to list with prompt
 var newItem = prompt("new item");
 var newPrice = prompt("new price");
-array01.push({name: newItem, price: newPrice});
-console.log(array01);
+newPrice = parseInt(newPrice);
+
+
+// if user entered price that is not a number
+
+// if(typeof newPrice == 'number'){
+// } else {
+//     alert("Please enter a valid price");
+
+// array01.push({
+//     name: newItem,
+//     price: newPrice
+// });
+// console.log(array01);
+// };
 
 // update total
-console.log("Updated Total: " + total.toFixed(2));
+runTotal();
+console.log("Updated Total: " + (total + newPrice).toFixed(2));
